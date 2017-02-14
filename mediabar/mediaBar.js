f1d28a3484730296dd7d177bf3a91bcdcf2ce041
moroniSena <lucasmoronidesena@gmail.com>
2017-02-14 15:58:12 -0700

    adding the code

---
+++ mediabar/mediaBar.js
@@ -0,0 +1,13 @@
+
+	var div = document.createElement('div');
+
+	div.className = 'mediaBarAside';
+
+	div.innerHTML = `<ul class="mediaBar">
+		<li><a href="https://www.facebook.com/"><img src="https://pbs.twimg.com/profile_images/3513354941/24aaffa670e634a7da9a087bfa83abe6_400x400.png"></a></li>
+		<li><a href="https://www.instagram.com/?hl=en"><img src="https://pbs.twimg.com/profile_images/730395088107372544/PgpnXhhh_400x400.jpg"></a></li>
+		<li><a href="https://www.twitter.com/?lang=en"><img src="https://pbs.twimg.com/profile_images/757561977988186113/1dJ-x4IE_400x400.jpg"></a></li>
+		<li><a href="https://www.google.com/"><img src="https://pbs.twimg.com/profile_images/748680372842471424/xnV12l6Z_400x400.jpg"></a></li>
+		</ul>`;
+
+	document.body.appendChild(div);
